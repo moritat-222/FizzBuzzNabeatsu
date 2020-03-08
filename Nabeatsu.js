a12f32ff5688681b96d43c4d82ce8ab722884065
moritat-222 <60217795+moritat-222@users.noreply.github.com>
2020-03-08 18:54:58 +0900

    ボタンを押すとスタートする機能を追加

--- Nabeatsu.js
+++ Nabeatsu.js
@@ -1,8 +1,11 @@
-for( i = 1; i < 100001; i++ ){
-    if ( i % 3 === 0 ){
-        document.write('<img width = "80" height = "100" src="https://pbs.twimg.com/media/Dz_5-z_UcAAg1bZ.jpg" alt ="アホになる「世界のナベアツ」氏">')
-    } 
-    else {
+const startButton = document.getElementById('start');
+
+startButton.onclick = () => {
+    for( i = 1; i < 100001; i++ ){
+        if ( i % 3 === 0 ){
+            document.write('<img width = "80" height = "100" src="https://pbs.twimg.com/media/Dz_5-z_UcAAg1bZ.jpg" alt ="アホになる「世界のナベアツ」氏">')
+        } else {
         document.write( i + ' ')
+        }
     }
-}
+};
